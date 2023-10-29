6d9e831a1c1c24553e76a6b6822301beef4c5469
Jack <32891072+JackDiWu@users.noreply.github.com>
2023-10-29 11:59:41 +0800

    z

--- defs.h
+++ defs.h
@@ -2,12 +2,13 @@
  * @Author: Jack
  * @Date: 2023-10-29 11:55:21
  * @LastEditors: Jack
- * @LastEditTime: 2023-10-29 11:55:42
+ * @LastEditTime: 2023-10-29 11:59:32
  */
 #ifndef _SNOW_DEFS_H_
 #define _SNOW_DEFS_H_
 
 #include <stdio.h>
 
+#include "lex.yy.h"
 
 #endif
