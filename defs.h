8d27c502d9c2d1a42c7ef22a562011b03ac4cc21
Jack <32891072+JackDiWu@users.noreply.github.com>
2023-10-29 14:26:42 +0800

    z

--- defs.h
+++ defs.h
@@ -2,7 +2,7 @@
  * @Author: Jack
  * @Date: 2023-10-29 11:55:21
  * @LastEditors: Jack
- * @LastEditTime: 2023-10-29 13:19:04
+ * @LastEditTime: 2023-10-29 14:26:15
  */
 #ifndef _SNOW_DEFS_H_
 #define _SNOW_DEFS_H_
@@ -12,8 +12,7 @@
 #include <string>
 
 extern "C" {
-    #include "snow.yy.h"
-    #include "lex.yy.h"
+    #include "parser.h"
 }
 
 #endif
--- parser.h
+++ parser.h
@@ -2,7 +2,7 @@
  * @Author: Jack
  * @Date: 2023-10-29 14:22:23
  * @LastEditors: Jack
- * @LastEditTime: 2023-10-29 14:23:46
+ * @LastEditTime: 2023-10-29 14:25:59
  */
 #ifndef _SNOW_PARSER_H_
 #define _SNOW_PARSER_H_
@@ -12,6 +12,7 @@
 
 typedef struct _yyparse_t {
     yyscan_t scanner;
+    
 } *yyparse_t;
 
 #endif
