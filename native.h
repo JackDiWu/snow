8781e4c57f323f10534f663c147e0506e16be7a4
Jack <32891072+JackDiWu@users.noreply.github.com>
2023-10-30 11:26:05 +0800

    Update native.h

--- native.h
+++ native.h
@@ -2,14 +2,13 @@
  * @Author: Jack
  * @Date: 2023-10-29 14:22:23
  * @LastEditors: Jack
- * @LastEditTime: 2023-10-30 11:18:29
+ * @LastEditTime: 2023-10-30 11:25:50
  */
 #ifndef _SNOW_NATIVE_H_
 #define _SNOW_NATIVE_H_
 
 typedef struct _yyparse_t {
     void *scanner;
-    
 } *yyparse_t;
 
 #endif
