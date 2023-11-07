7ff41ca2f882b946c565c02786de352e69bf298f
Jack <32891072+JackDiWu@users.noreply.github.com>
2023-11-07 10:01:33 +0800

    Update native.h

--- native.h
+++ native.h
@@ -2,12 +2,16 @@
  * @Author: Jack
  * @Date: 2023-11-07 09:52:17
  * @LastEditors: Jack
- * @LastEditTime: 2023-11-07 09:54:52
+ * @LastEditTime: 2023-11-07 10:01:27
  */
 #ifndef _SNOW_TYPES_H_
 #define _SNOW_TYPES_H_
 
 /***************** types ******************/
+
+
+
+/***************** defs ******************/
 #define YYSTYPE        long
 
 
