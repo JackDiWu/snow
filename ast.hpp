574969545074ce60ad81ee64113694c092f0188c
Jack <32891072+JackDiWu@users.noreply.github.com>
2023-11-07 10:47:20 +0800

    z

--- ast.hpp
+++ ast.hpp
@@ -22,10 +22,63 @@ namespace snow {
 
             std::shared_ptr<atom> L, R, T;
 
+            std::string tmp;
+
         public:
             atom(atom_type ap, yytoken_kind_t tp) : type(ap), token_type(tp) {}
 
             virtual ~atom() {}
+
+        public:
+            virtual const char * text() = 0;
+    };
+
+    class atom_sign : public atom {
+        public:
+            atom_sign(yytoken_kind_t tp) : atom(ATOM_TYPE_SIGN, tp) {
+
+            }
+
+            virtual ~atom_sign() {}
+
+        public:
+            virtual const char * text() {
+                switch (token_type) {
+                case TOKEN_EQUAL:
+                    return "=";
+                case TOKEN_PLUS:
+                    return "+";
+                case TOKEN_SUB:
+                    return "-";
+                case TOKEN_TIMES:
+                    return "*";
+                case TOKEN_DIVIDE:
+                    return "/";
+                case TOKEN_LA:
+                    return "<";
+                case TOKEN_RA:
+                    return ">";
+                case TOKEN_AND:
+                    return "&";
+                case TOKEN_OR:
+                    return "|";
+                case TOKEN_BROKEN_ISSUE:
+                    return "~";
+                case TOKEN_EXCLAMATION:
+                    return "!";
+                case TOKEN_PERCENT:
+                    return "%";
+                case TOKEN_XOR:
+                    return "^";
+                case TOKEN_LA_EQUAL:
+                    return "<=";
+                case TOKEN_RA_EQUAL:
+                    return ">=";
+                
+                default:
+                    return "unknow sign";
+                }
+            }
     };
 
     class atom_number : public atom {
@@ -48,6 +101,27 @@ namespace snow {
             atom_number(unsigned int v) : atom_number((uint64_t)v) {}
 
             virtual ~atom_number() {}
+
+        public:
+            virtual const char * text() {
+                char buffer[1024] = {};
+                switch (type) {
+                case ATOM_TYPE_INT: {
+                    snprintf(buffer, 1024, "%lld", v_int64); atom::tmp.assign(buffer);
+                    return tmp.c_str();
+                }
+                case ATOM_TYPE_UINT: {
+                    snprintf(buffer, 1024, "%llx", v_uint64); atom::tmp.assign(buffer);
+                    return tmp.c_str();
+                }
+                case ATOM_TYPE_FLOAT: {
+                    snprintf(buffer, 1024, "%lf", v_float64); atom::tmp.assign(buffer);
+                    return tmp.c_str();
+                }
+                default:
+                    return "unknow number";
+                }
+            }
     };
 
     class atom_string : public atom {
